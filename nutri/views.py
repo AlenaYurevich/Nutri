c71efae9b6e26b7a7659bddd704e7f48ecf11fe7
yurevich <yurevichei@mail.ru>
2025-07-14 22:13:27 +0300

    contact form

--- nutri/views.py
+++ nutri/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
-from calc.forms import ImtForm
+from .forms import ImtForm, ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 from calc.imt import calculate_bmi
 
 
 def main_view(request):
     if request.method == "POST":
         form = ImtForm(request.POST)
+        # form2 = ContactForm(request.POST)
         if form.is_valid():
             height = int(request.POST.get('height'))
             weight = float(request.POST.get('weight'))
             result = calculate_bmi(height, weight)
             context = {'form': form, 'result': result}
             return render(request, 'index.html', context)
+        # if form2.is_valid():
+        #     subject = 'Пробное сообщение'
+        #     body = {'name': form2.cleaned_data['name'],
+        #             'email': form2.cleaned_data['email'],
+        #             'phone': form2.cleaned_data['phone'],
+        #             'message': form2.cleaned_data['message'],
+        #             }
+        #     message = '\n'.join(body.values())
+        #     try:
+        #         send_mail(subject, message, 'admin@example.com', ['admin@example.com'])
+        #     except BadHeaderError:
+        #         return HttpResponse('Найден некорректный заголовок')
+        #     print('Сообщение отправлено')
     else:
         form = ImtForm()
+        form2 = ContactForm
         return render(request, 'index.html', {'form': form})  # внутри фигурных скобок
 
 
