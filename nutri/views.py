39a4cc11aba8e2a72a9154288d956bcfe5c679f2
yurevich <yurevichei@mail.ru>
2025-07-17 17:14:41 +0300

    contact form

--- nutri/views.py
+++ nutri/views.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 def index(request):
     bmi_result = None
     contact_success = False
-    bmi_form = BMIForm()
+    bmi_form = BMIForm(request.POST if 'bmi_submit' in request.POST else None)
     contact_form = ContactForm()
     # Обработка формы ИМТ
     if 'bmi_submit' in request.POST:
@@ -17,19 +17,26 @@ def index(request):
             height = bmi_form.cleaned_data['height']
             weight = bmi_form.cleaned_data['weight']
             bmi_result = calculate_bmi(height, weight)
-            # Не валидна - форма сохранит ошибки
-        # else:
-        #     bmi_form = BMIForm()
-            # Обработка контактной формы
+
+    # Обработка контактной формы
     elif 'contact_submit' in request.POST:
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             # Сохранение данных и отправка email
-            contact = contact_form.save()
+            # contact = contact_form.save()
+            name = contact_form.cleaned_data['name']
+            phone = contact_form.cleaned_data['phone']
+            email = contact_form.cleaned_data['email']
+            message = contact_form.cleaned_data['message']
+            print("Форма валидна! Данные:")
+            print(f"Имя: {contact_form.cleaned_data['name']}")
+            print(f"Телефон: {contact_form.cleaned_data['phone']}")
+            print(f"Email: {contact_form.cleaned_data['email']}")
+            print(f"Сообщение: {contact_form.cleaned_data['message']}")
             # Отправка email (пример)
             send_mail(
                 'Новая заявка с сайта',
-                f'Имя: {contact.name}\nТелефон: {contact.phone}\nСообщение: {contact.message}',
+                f'Имя: {name}\nТелефон: {phone}\nEmail: {phone}\nСообщение: {message}',
                 'noreply@example.com',
                 ['you@example.com'],
                 fail_silently=False,
@@ -38,7 +45,8 @@ def index(request):
             contact_form = ContactForm()  # Очищаем форму после успешной отправки
             # Не валидна - форма сохранит ошибки
         else:
-            contact_form = ContactForm()
+            print("Ошибки формы:", contact_form.errors)
+
     context = {
         'bmi_form': bmi_form,
         'bmi_result': bmi_result,
@@ -50,7 +58,3 @@ def index(request):
 
 def about_view(request):
     return render(request, 'about.html')
-#
-#
-# def page_not_found_view(request):
-#     return render(request, '404/404.html', status=404)
