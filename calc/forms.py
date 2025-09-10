b362f2b3c4cc7a6417d9046897e01a2d36af721d
yurevich <yurevichei@mail.ru>
2025-09-10 20:46:42 +0300

    расчеты нутрициолога

--- calc/forms.py
+++ calc/forms.py
@@ -7,6 +7,8 @@ Choices_notice = [(1, f'Женщина'),
 class IdmtForm(forms.Form):
     height = forms.IntegerField(label="Введите свой рост в сантиметрах", min_value=120, initial=170,
                                 widget=forms.NumberInput(attrs={'class': "form-control", 'autofocus': 'autofocus'}))
+    weight = forms.DecimalField(label="Введите свой вес", min_value=1, initial=60,
+                                decimal_places=1, widget=forms.NumberInput(attrs={'class': "form-control"}))
     gender = forms.TypedChoiceField(label="Пол", choices=Choices_notice, initial=1,
                                           widget=forms.RadioSelect())
 
